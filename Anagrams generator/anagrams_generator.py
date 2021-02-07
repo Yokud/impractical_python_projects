1731382e5cff9fb5aa8cbe12ac2460389b4e7e5e
Malyshev Ivan <thefuny@mail.ru>
2021-02-07 03:09:57 +0300

    complete anagrams generator

--- Anagrams generator/anagrams_generator.py
+++ Anagrams generator/anagrams_generator.py
@@ -1,25 +1,20 @@
-"""User enters name and interactively derives anagram phrase from name.
+"""
+User enters name and interactively derives anagram phrase from name.
+
 Requires access to an external English word dictionary file
 and file-loading module "load_dictionary.py"
 """
+
 from collections import Counter
-from itertools import permutations
+from itertools import combinations
 import load_dictionary
 
-dict_file = load_dictionary.load('words.txt')
-# ensure "a" & "I" are included
-dict_file.append('a')
-dict_file.append('i')
-dict_file = sorted(dict_file)
-
-ini_name = input("Enter a name: ")
-
-#______________________________________________________________________________
 
 def find_anagrams(name, word_list):
-    """Read name & dictionary file & display all anagrams IN name."""
+    """Read name & dictionary file & display all anagrams in name."""
     name_letter_map = Counter(name)
     anagrams = []
+
     for word in word_list:
         test = ''
         word_letter_map = Counter(word.lower())
@@ -36,42 +31,57 @@ def find_anagrams(name, word_list):
     return anagrams
 
 
-def phrases_filter(name, phrases):
-    """Filter only phrases with the same letters"""
+def phrases_gen(name, word_list):
+    """Generate anagram phrases from name."""
     name_letters = Counter(name)
+    filtered = []
 
+    phrases = set()
+    while len(name):
+        anagrams = find_anagrams(name, word_list)
+        phrases |= set(anagrams)
+        name = name[1:]
+
+    combs = set()
+    temp_combs = set(combinations(phrases, 1))
+    temp = 1
+    while len(temp_combs) or temp < 2:
+        combs |= temp_combs
+        temp_combs = set(combinations(phrases, temp))
+        temp += 1
+
+
+    phrases = [' '.join(c) for c in combs]
     for phrase in phrases:
         phrase_letters = Counter(phrase.replace(' ', ''))
 
-        if phrase_letters != name_letters:
-            phrases.remove(phrase)
+        if phrase_letters == name_letters:
+            filtered.append(phrase)
 
-    return phrases
+    return filtered
 
 
 def main():
-    """Help user build anagram phrase from their name."""
-    # remove spaces & convert to lower case
-    name = ''.join(ini_name.lower().split())
-    # remove hyphens in hyphenated names
-    name = name.replace('-', '')
+    """Load words, format name, get anagrams and print first no more 500 phrases."""
+    dict_file = load_dictionary.load('words.txt')
 
-    phrases = list()
+    # ensure "a" & "I" are included
+    dict_file.append('a')
+    dict_file.append('i')
 
-    while len(name):
-        anagrams = find_anagrams(name, dict_file)
-        phrases.extend(anagrams)
-        phrases = list(set(phrases))
-        name = name[1:]
+    dict_file = sorted(dict_file)
 
-    print(phrases)
-    phrases = [' '.join(p) for p in permutations(phrases)]
+    ini_name = input("Enter a name: ")
+    name = ''.join(ini_name.lower().split())  # remove spaces & convert to lower case
+    name = name.replace('-', '')  # remove hyphens in hyphenated names
 
-    name = ''.join(ini_name.lower().split())
-    phrases = phrases_filter(name, phrases)
-    print(*phrases, sep='\n')
 
-#______________________________________________________________________________
+    phrases = phrases_gen(name, dict_file)
+    print(len(phrases))
+
+    for i in range(500 if len(phrases) > 500 else len(phrases)):
+        print(phrases[i])
+
 
 if __name__ == '__main__':
     main()
